8bfabe62fd5fb7c0e5d83c7648bc93618ce59af5
Leonardo Porcelli <61625009+Lepo28@users.noreply.github.com>
2023-11-29 16:25:12 +0100

    Useless shift removed

--- src/main/java/org/apache/commons/imaging/formats/tiff/photometricinterpreters/PhotometricInterpreterYCbCr.java
+++ src/main/java/org/apache/commons/imaging/formats/tiff/photometricinterpreters/PhotometricInterpreterYCbCr.java
@@ -50,7 +50,7 @@ public class PhotometricInterpreterYCbCr extends PhotometricInterpreter {
         final int b = limit((int) b1, 0, 255);
 
         final int alpha = 0xff;
-        return (alpha << 24) | (r << 16) | (g << 8) | (b << 0);
+        return (alpha << 24) | (r << 16) | (g << 8) | b;
     }
 
     public static int limit(final int value, final int min, final int max) {
