88768285546bb4b9dd5e04df0c967900f5bf070a
Leonardo Porcelli <61625009+Lepo28@users.noreply.github.com>
2023-11-29 17:04:58 +0100

    Useless shift removed

--- src/main/java/org/apache/commons/imaging/palette/ColorSpaceSubset.java
+++ src/main/java/org/apache/commons/imaging/palette/ColorSpaceSubset.java
@@ -166,7 +166,7 @@ final class ColorSpaceSubset {
         redsum /= total;
         greensum /= total;
         bluesum /= total;
-        rgb = (int) (((redsum & 0xff) << 16) | ((greensum & 0xff) << 8) | ((bluesum & 0xff) << 0));
+        rgb = (int) (((redsum & 0xff) << 16) | ((greensum & 0xff) << 8) | ((bluesum & 0xff)));
     }
 
     public void setIndex(final int i) {
